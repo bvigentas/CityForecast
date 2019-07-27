8e49800961cc3d2927d6b0376b4cb3ceb937e711
Bruno Vigentas <brunovigentas@gmail.com>
2019-07-26 23:17:48 -0300

    Initial commit

---
+++ back-end/src/main/java/br/com/brunovigentas/hbsisproject/HbsisProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.brunovigentas.hbsisproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbsisProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HbsisProjectApplication.class, args);
+    }
+
+}
---
+++ back-end/src/test/java/br/com/brunovigentas/hbsisproject/HbsisProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.brunovigentas.hbsisproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HbsisProjectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
